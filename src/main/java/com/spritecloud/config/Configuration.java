4520b939f7502670be44feb3c07717e2a2976805
arberbakalli <arberbakalli@gmail.com>
2025-06-07 12:55:15 +0200

    added configuration,APIs,Secrets and such in properties files and base API class to be reused for flexibility

---
+++ src/main/java/com/spritecloud/config/Configuration.java
@@ -0,0 +1,42 @@
+package com.spritecloud.config;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.LoadPolicy;
+import org.aeonbits.owner.Config.LoadType;
+
+@LoadPolicy(LoadType.MERGE)
+@Config.Sources({
+    "system:properties",
+    "classpath:api.properties"})
+public interface Configuration extends Config{
+
+    @Key("api.base.path")
+    String basePath();
+
+    @Key("api.base.uri")
+    String baseURI();
+
+    @Key("api.superSecret.key")
+    String key();
+
+    @Key("api.superSecret.value")
+    String value();
+
+    @Key("log.all")
+    boolean logAll();
+
+    @Key("superSecret.userName")
+    String userName();
+
+    @Key("superSecret.password")
+    String password();
+
+    @Key("superSecret.expectedToken")
+    String expectedToken();
+
+    @Key("ui.superSecret.superAdmin")
+    String superAdmin();
+
+    @Key("ui.superSecret.superPassword")
+    String superPassword();
+}
---
+++ src/test/java/com/spritecloud/BaseAPI.java
@@ -0,0 +1,44 @@
+package com.spritecloud;
+
+import com.spritecloud.config.Configuration;
+import com.spritecloud.config.ConfigurationManager;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public abstract class BaseAPI {
+
+    protected static Configuration configuration;
+    protected static RequestSpecification baseRequest;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        configuration = ConfigurationManager.getConfiguration();
+
+        baseURI = configuration.baseURI();
+        basePath = configuration.basePath();
+
+        baseRequest = given()
+                .header(configuration.key(), configuration.value())
+                .contentType(ContentType.JSON);
+
+        RestAssured.useRelaxedHTTPSValidation();
+
+        determineLog();
+    }
+
+    private static void determineLog() {
+        if (configuration.logAll()) {
+            RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        } else {
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        }
+    }
+}
